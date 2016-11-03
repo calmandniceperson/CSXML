864d9b87cbf6414a405a600d690993980ae94910
karina15 <karina15@users.noreply.github.com>
2016-11-03 11:42:29 +0100

    Add files via upload

---
+++ XMLTextReaderWriter/ConsoleApplication/Park.cs
@@ -0,0 +1,30 @@
+﻿using System.Collections.Generic;
+using System.Text;
+
+namespace ConsoleApplication
+{
+    public class Park
+    {
+        public int ID { get; set; }
+        public string Name { get; set; }
+        
+        public List<Tree> TreeList { get; private set; }
+        
+        public Park()
+        {
+            TreeList = new List<Tree>();
+        }
+        
+        override public string ToString()
+        {
+            StringBuilder sb = new StringBuilder();           
+            sb.Append("=== Park ===\n")
+                .Append("Name: "+ this.Name + "\n");
+            foreach (var t in TreeList)
+            {
+                sb.Append(t.ToString());
+            }
+            return sb.ToString();
+        }
+    }
+}
---
+++ XMLTextReaderWriter/ConsoleApplication/XmlTextWriting.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Xml;
+
+namespace ConsoleApplication
+{
+    class XmlTextWriting
+    {
+        public void Write(List<Park> parks, string dest)
+        {
+            XmlTextWriter writer = new XmlTextWriter(dest,null);
+            writer.WriteStartElement("RootNode");
+            foreach (Park p in parks)
+            {
+                writer.WriteStartElement("park");
+                if (p.ID != -1)
+                    writer.WriteAttributeString("id", p.ID.ToString());
+                foreach (Tree t in p.TreeList)
+                {
+                    writer.WriteStartElement("tree");
+                    if (t.ID != -1)
+                        writer.WriteAttributeString("id", t.ID.ToString());
+                    writer.WriteElementString("age", t.AgeInYears.ToString());
+                    writer.WriteElementString("type", t.Type);
+                    writer.WriteEndElement();
+                }
+                writer.WriteEndElement();
+            }
+            writer.WriteEndElement();
+            writer.Close();
+        }
+    }
+}
