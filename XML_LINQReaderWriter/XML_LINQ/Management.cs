3f4d3dc47f72f49dd3038684472bd38b1b7c5b1c
Alexander <nethane.lithium@gmail.com>
2016-11-04 13:00:13 +0100

    Add LINQ ReaderWriter
    Add LINQ ReaderWriter

---
+++ XML_LINQReaderWriter/XML_LINQ/Management.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace XML_LINQ {
+    class Management {
+        private static Management instance;
+
+        private Management() {
+            this.ParkList = new List<Park>();
+        }
+
+        public static Management Instance {
+            get {
+                if (instance == null) {
+                    instance = new Management();
+                }
+                return instance;
+            }
+        }
+
+        public List<Park> ParkList { get; }
+
+        public void Print() {
+            ParkList.ForEach(x => { Console.WriteLine(x.ToString()); });
+        }
+    }
+}
---
+++ XML_LINQReaderWriter/XML_LINQ/Program.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Xml;
+using System.Threading.Tasks;
+using System.Xml.Linq;
+
+namespace XML_LINQ {
+    class Program {
+        public static string rootAttribute = "ID";
+        static void Main(string[] args) {
+            Console.Clear();
+            // Read Koczwara
+            XMLReaderLINQ.Read("baum.xml");
+            // Allgemeines Printen des Eingelesenen
+            Management.Instance.Print();
+            // Write Koczwara
+            XMLWriterLINQ.Write(Management.Instance.ParkList, "out.xml");
+
+            Console.ReadLine();
+        }
+
+        private static string LINQt(XElement xelement) {
+            StringBuilder result = new StringBuilder();
+            IEnumerable<XElement> parks = xelement.Elements();
+            foreach (var park in parks) {
+                result.Append(park.Value);
+                if(park.HasAttributes)
+                    result.Append(" ID=" + park.Attribute("id").Value);
+                result.Append("\n\r");
+            }
+            return result.ToString();
+        }
+    }
+}
